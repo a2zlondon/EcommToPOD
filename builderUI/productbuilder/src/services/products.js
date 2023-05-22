b1fd60c523305dcb02f1428f415c2a557b0ead47
Daniel Molloy <d4nmolloy@gmail.com>
2023-05-22 10:34:18 +0100

    uibuild

--- builderUI/productbuilder/src/services/products.js
+++ builderUI/productbuilder/src/services/products.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-const baseUrl = '/api/products'
+//const baseUrl = '/api/products'
 //const baseUrl = 'http://localhost:3000/api/products'
-//const baseUrl = 'https://ecomm-to-pod.onrender.com/api/products'
+const baseUrl = 'https://ecomm-to-pod.onrender.com/api/products'
 
 const getAll = (id) => {
   console.log(`ID = = =${id}`)
